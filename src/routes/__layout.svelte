e4f5cea21821c03ca980ac50106f2d93e791e4cd
Fernando <ferobregon03@gmail.com>
2022-07-01 23:16:17 +0100

    Added SEO Support;

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,9 @@
 <script>
-	import '$assets/app.css';
-	import { onMount } from 'svelte';
 	import { themeChange } from 'theme-change';
+	import SvelteSeo from 'svelte-seo';
+
+	import { onMount } from 'svelte';
+	import '$assets/app.css';
 
 	// NOTE: the element that is using one of the theme attributes must be in the DOM on mount
 	onMount(() => {
@@ -10,5 +12,9 @@
 	});
 </script>
 
+<SvelteSeo
+	title="Fernando Obregon - Developer and Creator"
+	description="Developer Traveling around the world sharing Knowledge and Experiences"
+/>
 <slot><!-- optional fallback --></slot>
 <div class="absolute bottom-4 right-4 w-16 h-16 rounded-full bg-secondary" />
---
+++ src/routes/blog/index.svelte
@@ -0,0 +1,11 @@
+<script>
+	import SvelteSeo from 'svelte-seo';
+</script>
+
+<SvelteSeo
+	title="Blog - Fernando Obregon "
+	description="Developer Traveling around the world sharing Knowledge and Experiences"
+/>
+<div>
+	<h1>Blog</h1>
+</div>
